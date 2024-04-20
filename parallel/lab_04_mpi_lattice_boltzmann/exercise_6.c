a6668976f1390dbdea0a678b74f4a3c7f419f519
Andrei Ilin <stariktenger@gmail.com>
2024-04-20 13:43:55 +0200

    parallel: ex6

--- parallel/lab_04_mpi_lattice_boltzmann/exercise_6.c
+++ parallel/lab_04_mpi_lattice_boltzmann/exercise_6.c
@@ -23,6 +23,7 @@
 /****************************************************/
 #include "src/lbm_struct.h"
 #include "src/exercises.h"
+#include  <stdbool.h> 
 
 /****************************************************/
 void lbm_comm_init_ex6(lbm_comm_t * comm, int total_width, int total_height)
@@ -41,30 +42,195 @@ void lbm_comm_release_ex6(lbm_comm_t * comm)
 /****************************************************/
 void lbm_comm_ghost_exchange_ex6(lbm_comm_t * comm, lbm_mesh_t * mesh)
 {
-	//
-	// TODO: Implement the 2D communication with :
-	//         - non-blocking MPI functions
-	//         - use MPI type for non contiguous side 
-	//
-	// To be used:
-	//    - DIRECTIONS: the number of doubles composing a cell
-	//    - double[9] lbm_mesh_get_cell(mesh, x, y): function to get the address of a particular cell.
-	//    - comm->width : The with of the local sub-domain (containing the ghost cells)
-	//    - comm->height : The height of the local sub-domain (containing the ghost cells)
-	//
-	// TIP: create a function to get the target rank from x,y task coordinate.
-	// TIP: You can use MPI_PROC_NULL on borders.
-	// TIP: send the corner values 2 times, with the up/down/left/write communication
-	//      and with the diagonal communication in a second time, this avoid
-	//      special cases for border tasks.
-	// TIP: The previous trick require to make two batch of non-blocking communications.
+	// ======== PREPARATION ========
 
-	//example to access cell
-	//double * cell = lbm_mesh_get_cell(mesh, local_x, local_y);
-	//double * cell = lbm_mesh_get_cell(mesh, comm->width - 1, 0);
+	// Left-right borders
+	double *border_l = lbm_mesh_get_cell(mesh, 1, 0);
+	double *border_r = lbm_mesh_get_cell(mesh, comm->width - 2, 0);
+	double *ghost_l = lbm_mesh_get_cell(mesh, 0, 0);
+	double *ghost_r = lbm_mesh_get_cell(mesh, comm->width - 1, 0);
 
-	//TODO:
-	//   - implement left/write communications
-	//   - implement top/bottom communication (non contiguous)
-	//   - implement diagonal communications
+	// Corners
+	double *corner_top_left = lbm_mesh_get_cell(mesh, 1, 1);
+	double *corner_top_right = lbm_mesh_get_cell(mesh, comm->width - 2, 1);
+	double *corner_bottom_left = lbm_mesh_get_cell(mesh, 1, comm->height - 2);
+	double *corner_bottom_right = lbm_mesh_get_cell(mesh, comm->width - 2, 
+													comm->height - 2);
+
+	double *ghost_top_left = lbm_mesh_get_cell(mesh, 0, 0);
+	double *ghost_top_right = lbm_mesh_get_cell(mesh, comm->width - 1, 0);
+	double *ghost_bottom_left = lbm_mesh_get_cell(mesh, 0, comm->height - 1);
+	double *ghost_bottom_right = lbm_mesh_get_cell(mesh, comm->width - 1, 
+												   comm->height - 1);
+	// ======== EXCHANGE ========
+
+	const size_t VSIZE = (comm->height) * DIRECTIONS;
+	const size_t HSIZE = (comm->width);
+	MPI_Request request_left;
+	MPI_Request request_right;
+	MPI_Request request_top;
+	MPI_Request request_bottom;
+	MPI_Request request_top_right;
+	MPI_Request request_top_left;
+	MPI_Request request_bottom_left;
+	MPI_Request request_bottom_right;
+
+	MPI_Request recv_left;
+	MPI_Request recv_right;
+	MPI_Request recv_top;
+	MPI_Request recv_bottom;
+	MPI_Request recv_top_right;
+	MPI_Request recv_top_left;
+	MPI_Request recv_bottom_left;
+	MPI_Request recv_bottom_right;
+
+	const bool nb_left = comm->rank_x - 1 >= 0;
+	const bool nb_right = comm->rank_x + 1 < comm->nb_x;
+	const bool nb_top = comm->rank_y - 1 >= 0;
+	const bool nb_bottom = comm->rank_y + 1 < comm->nb_y;
+
+	// -------- SEND SIDES --------
+
+	// Left
+	if (nb_left) {
+		MPI_Isend(border_l, VSIZE, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x - 1, comm->rank_y), 
+			0, MPI_COMM_WORLD, &request_left);
+	}
+
+	// Right
+	if (nb_right) {
+		MPI_Isend(border_r, VSIZE, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x + 1, comm->rank_y),
+			0, MPI_COMM_WORLD, &request_right);
+	}
+
+	// Top
+	if (nb_top) {
+		MPI_Isend(lbm_mesh_get_cell(mesh, 0, 1), 1, comm->type, 
+			get_rank(comm, comm->rank_x, comm->rank_y - 1),
+			0, MPI_COMM_WORLD, &request_top);
+	}
+
+	// Bottom
+	if (nb_bottom) {
+		MPI_Isend(lbm_mesh_get_cell(mesh, 0, comm->height - 2), 1, comm->type, 
+			get_rank(comm, comm->rank_x, comm->rank_y + 1),
+			0, MPI_COMM_WORLD, &request_bottom);
+	}
+
+
+	// -------- RECEIVE SIDES --------
+
+	// Top
+	if (nb_top) {
+		MPI_Irecv(lbm_mesh_get_cell(mesh, 0, 0), 1, comm->type, 
+			get_rank(comm, comm->rank_x, comm->rank_y - 1),
+			0, MPI_COMM_WORLD, &recv_top);
+	}
+
+	// Bottom
+	if (nb_bottom) {
+		MPI_Irecv(lbm_mesh_get_cell(mesh, 0, comm->height - 1), 1, comm->type, 
+			get_rank(comm, comm->rank_x, comm->rank_y + 1),
+			0, MPI_COMM_WORLD, &recv_bottom);
+	}
+
+	// Left
+	if (nb_left) {
+		MPI_Irecv(ghost_l, VSIZE, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x - 1, comm->rank_y), 
+			0, MPI_COMM_WORLD, &recv_left);
+	}
+
+	// Right
+	if (nb_right) {
+		MPI_Irecv(ghost_r, VSIZE, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x + 1, comm->rank_y),
+			0, MPI_COMM_WORLD, &recv_right);
+	}
+
+	// --------  SEND CORNERS --------
+
+	// Top-left corner
+	if (nb_top  && nb_left) {
+		MPI_Isend(corner_top_left, DIRECTIONS, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x - 1, comm->rank_y - 1),
+			0, MPI_COMM_WORLD, &request_top_left);
+	}
+
+	// Bottom-left corner
+	if (nb_bottom && nb_left) {
+		MPI_Isend(corner_bottom_left, DIRECTIONS, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x - 1, comm->rank_y + 1),
+			0, MPI_COMM_WORLD, &request_bottom_left);
+	}
+
+	// Top-right corner
+	if (nb_top && nb_right) {
+		MPI_Isend(corner_top_right, DIRECTIONS, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x + 1, comm->rank_y - 1),
+			0, MPI_COMM_WORLD, &request_top_right);
+	}
+
+	// Bottom-right corner
+	if (nb_bottom && nb_right) {
+		MPI_Isend(corner_bottom_right, DIRECTIONS, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x + 1, comm->rank_y + 1),
+			0, MPI_COMM_WORLD, &request_bottom_right);
+	}
+
+	if (nb_left) MPI_Wait(&request_left, MPI_STATUS_IGNORE);
+	if (nb_right) MPI_Wait(&request_right, MPI_STATUS_IGNORE);
+	if (nb_top) MPI_Wait(&request_top, MPI_STATUS_IGNORE);
+	if (nb_bottom) MPI_Wait(&request_bottom, MPI_STATUS_IGNORE);
+
+	if (nb_left) MPI_Wait(&recv_left, MPI_STATUS_IGNORE);
+	if (nb_right) MPI_Wait(&recv_right, MPI_STATUS_IGNORE);
+	if (nb_top) MPI_Wait(&recv_top, MPI_STATUS_IGNORE);
+	if (nb_bottom) MPI_Wait(&recv_bottom, MPI_STATUS_IGNORE);
+
+	// -------- RECEIVE CORNERS --------
+
+	// Top-left corner
+	if (nb_top && nb_left) {
+		MPI_Irecv(ghost_top_left, DIRECTIONS, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x - 1, comm->rank_y - 1),
+			0, MPI_COMM_WORLD, &recv_top_left);
+	}
+
+	// Bottom-left corner
+	if (nb_bottom && nb_left) {
+		MPI_Irecv(ghost_bottom_left, DIRECTIONS, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x - 1, comm->rank_y + 1),
+			0, MPI_COMM_WORLD, &recv_bottom_left);
+	}
+	
+	// Top-right corner
+	if (nb_top && nb_right) {
+		MPI_Irecv(ghost_top_right, DIRECTIONS, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x + 1, comm->rank_y - 1),
+			0, MPI_COMM_WORLD, &recv_top_right);
+	}
+
+	// Bottom-right corner
+	if (nb_bottom && nb_right) {
+		MPI_Irecv(ghost_bottom_right, DIRECTIONS, MPI_DOUBLE, 
+			get_rank(comm, comm->rank_x + 1, comm->rank_y + 1),
+			0, MPI_COMM_WORLD, &recv_bottom_right);
+	}
+
+	if (nb_top && nb_right) MPI_Wait(&request_top_right, MPI_STATUS_IGNORE);
+	if (nb_bottom && nb_right) MPI_Wait(&request_bottom_right, MPI_STATUS_IGNORE);
+	if (nb_top && nb_left) MPI_Wait(&request_top_left, MPI_STATUS_IGNORE);
+	if (nb_bottom && nb_left) MPI_Wait(&request_bottom_left, MPI_STATUS_IGNORE);
+
+	if (nb_top && nb_right) MPI_Wait(&recv_top_right, MPI_STATUS_IGNORE);
+	if (nb_bottom && nb_right) MPI_Wait(&recv_bottom_right, MPI_STATUS_IGNORE);
+	if (nb_top && nb_left) MPI_Wait(&recv_top_left, MPI_STATUS_IGNORE);
+	if (nb_bottom && nb_left) MPI_Wait(&recv_bottom_left, MPI_STATUS_IGNORE);
+
+	
+
+	// ==========================
 }
